7de9488dbfb2f5a3474629ecabd2d81f14f80c55
sorgelig <pour.garbage@gmail.com>
2020-08-15 03:14:55 +0800

    joytick: fixes and tweaks.

--- rtl/soc/joystick.v
+++ rtl/soc/joystick.v
@@ -1,26 +1,3 @@
-`timescale 1 ps / 1 ps
-module joystick
-(
-	input         rst_n,
-	input         clk,
-	input         clk_grav,
-	input [13:0]  dig_1,
-	input [13:0]  dig_2,
-	input [15:0]  ana_1,
-	input [15:0]  ana_2,
-	input  [1:0]  mode,
-
-	output reg [7:0] readdata,
-	input         write
-);
-
-always @(posedge clk) readdata <= {jb4, jb3, jb2, jb1, JOY2_Y>0, JOY2_X>0, JOY1_Y>0, JOY1_X>0};
-
-reg [8:0] JOY1_X;
-reg [8:0] JOY1_Y;
-
-reg [8:0] JOY2_X;
-reg [8:0] JOY2_Y;
 
 
 // When using the 90.5 MHz clk, an 8-bit CLK_DIV and 9-bit X/Y value gives a reading of around 1400 microseconds using the JoyCheck DOS program (by Henrik K Jensen).
@@ -77,7 +54,25 @@ reg [8:0] JOY2_Y;
 // 20khz clock.
 // 2262 to handle highs and lows.
 
-reg [8:0] CLK_DIV;
+module joystick
+(
+	input         rst_n,
+	input         clk,
+	input         clk_grav,
+	input [13:0]  dig_1,
+	input [13:0]  dig_2,
+	input [15:0]  ana_1,
+	input [15:0]  ana_2,
+	input  [1:0]  mode,
+
+	output reg [7:0] readdata,
+	input         write
+);
+
+always @(posedge clk) readdata <= {jb4, jb3, jb2, jb1, |JOY2_Y, |JOY2_X, |JOY1_Y, |JOY1_X};
+
+reg  [7:0] JOY1_X,JOY1_Y,JOY2_X,JOY2_Y;
+reg [10:0] CLK_DIV;
 
 wire JOY1_RIGHT = dig_1[0];
 wire JOY1_LEFT  = dig_1[1];
@@ -109,36 +104,25 @@ wire JOY2_L1    = dig_2[11];
 wire JOY2_R2    = dig_2[12];
 wire JOY2_L2    = dig_2[13];
 
-reg jb1 = 1;
-reg jb2 = 1;
-reg jb3 = 1;
-reg jb4 = 1;
-
-wire [8:0] j1x_r  = {ana_1[7],ana_1[7:0]};
-wire [8:0] j1y_r  = {ana_1[15],ana_1[15:8]};
-wire [8:0] j2x_r  = {ana_2[7],ana_2[7:0]};
-wire [8:0] j2y_r  = {ana_2[15],ana_2[15:8]};
-
-wire [8:0] j1x = j1x_r + {j1x_r[8],j1x_r[8:1]} + 9'd200;
-wire [8:0] j1y = j1y_r + {j1y_r[8],j1y_r[8:1]} + 9'd200;
-wire [8:0] j2x = j2x_r + {j2x_r[8],j2x_r[8:1]} + 9'd200;
-wire [8:0] j2y = j2y_r + {j2y_r[8],j2y_r[8:1]} + 9'd200;
-
+reg       jb1, jb2, jb3, jb4;
 reg [1:0] gravis_out = 0;
-reg gravis_clk = 0;
+reg       gravis_clk = 0;
 
 always @(posedge clk) begin : joy_block
 	reg [4:0] gravis_pos;
+	reg use_dpad1, use_dpad2;
 
 	if (!rst_n) begin
-		JOY1_X <= 9'd197;
-		JOY1_Y <= 9'd197;
-		JOY2_X <= 9'd197;
-		JOY2_Y <= 9'd197;
+		JOY1_X <= 128;
+		JOY1_Y <= 128;
+		JOY2_X <= 128;
+		JOY2_Y <= 128;
 		gravis_clk <= 0;
 		gravis_out <= 0;
 		gravis_pos <= 0;
 		{jb1, jb2, jb3, jb4} <= 4'b1111;
+		use_dpad1 <= 1;
+		use_dpad2 <= 1;
 	end
 	else begin
 		jb1 <= mode == 2 ? gravis_clk : !JOY1_BUT1;
@@ -151,47 +135,56 @@ always @(posedge clk) begin : joy_block
 			gravis_pos <= gravis_pos == 23 ? 5'd0 : gravis_pos + 5'd1;
 
 			case (gravis_pos)
-				0, 6, 11, 16, 21: gravis_out <= 0;
-				1, 2, 3, 4, 5:    gravis_out <= 1;
+				0, 6, 11, 16, 21:
+				    gravis_out <= 0;
 
-				7:                gravis_out <= {JOY2_SEL,   JOY1_SEL};
-				8:                gravis_out <= {JOY2_START, JOY1_START};
-				9:                gravis_out <= {JOY2_R2,    JOY1_R2};
-				10:               gravis_out <= {JOY2_BUT4,  JOY1_BUT4};
+				1, 2, 3, 4, 5:
+				    gravis_out <= 1;
 
-				12:               gravis_out <= {JOY2_L2,    JOY1_L2};
-				13:               gravis_out <= {JOY2_BUT2,  JOY1_BUT2};
-				14:               gravis_out <= {JOY2_BUT1,  JOY1_BUT1};
-				15:               gravis_out <= {JOY2_BUT3,  JOY1_BUT3};
+				 7: gravis_out <= {JOY2_SEL,   JOY1_SEL};
+				 8: gravis_out <= {JOY2_START, JOY1_START};
+				 9: gravis_out <= {JOY2_R2,    JOY1_R2};
+				10: gravis_out <= {JOY2_BUT4,  JOY1_BUT4};
 
-				17:               gravis_out <= {JOY2_L1,    JOY1_L1};
-				18:               gravis_out <= {JOY2_R1,    JOY1_R1};
-				19:               gravis_out <= {JOY2_UP,    JOY1_UP};
-				20:               gravis_out <= {JOY2_DOWN,  JOY1_DOWN};
+				12: gravis_out <= {JOY2_L2,    JOY1_L2};
+				13: gravis_out <= {JOY2_BUT2,  JOY1_BUT2};
+				14: gravis_out <= {JOY2_BUT1,  JOY1_BUT1};
+				15: gravis_out <= {JOY2_BUT3,  JOY1_BUT3};
 
-				22:               gravis_out <= {JOY2_RIGHT, JOY1_RIGHT};
-				23:               gravis_out <= {JOY2_LEFT,  JOY1_LEFT};
+				17: gravis_out <= {JOY2_L1,    JOY1_L1};
+				18: gravis_out <= {JOY2_R1,    JOY1_R1};
+				19: gravis_out <= {JOY2_UP,    JOY1_UP};
+				20: gravis_out <= {JOY2_DOWN,  JOY1_DOWN};
+
+				22: gravis_out <= {JOY2_RIGHT, JOY1_RIGHT};
+				23: gravis_out <= {JOY2_LEFT,  JOY1_LEFT};
 			endcase
 		end
+		
+		if((ana_1[7:0] > 100 && ana_1[7:0] < 156) || (ana_1[15:8] > 100 && ana_1[15:8] < 156)) use_dpad1 <= 0;
+		if(dig_1[3:0]) use_dpad1 <= 1;
+
+		if((ana_2[7:0] > 100 && ana_2[7:0] < 156) || (ana_2[15:8] > 100 && ana_2[15:8] < 156)) use_dpad2 <= 0;
+		if(dig_2[3:0]) use_dpad2 <= 1;
 
 		CLK_DIV <= CLK_DIV + 1'b1;
 
 		if (write) begin
-			JOY1_X <= j1x_r ? j1x : (JOY1_LEFT) ? 9'd8 : (JOY1_RIGHT) ? 9'd391 : 9'd200;
-			JOY1_Y <= j1y_r ? j1y : (JOY1_UP)   ? 9'd8 : (JOY1_DOWN)  ? 9'd391 : 9'd200;
+			JOY1_X <= ~use_dpad1 ? {~ana_1[7],  ana_1[6:0]}  : JOY1_LEFT  ? 8'd4 : JOY1_RIGHT ? 8'd252 : 8'd128;
+			JOY1_Y <= ~use_dpad1 ? {~ana_1[15], ana_1[14:8]} : JOY1_UP    ? 8'd4 : JOY1_DOWN  ? 8'd252 : 8'd128;
 
-			JOY2_X <= j2x_r ? j2x : (JOY2_LEFT) ? 9'd8 : (JOY2_RIGHT) ? 9'd391 : 9'd200;
-			JOY2_Y <= j2y_r ? j2y : (JOY2_UP)   ? 9'd8 : (JOY2_DOWN)  ? 9'd391 : 9'd200;
+			JOY2_X <= ~use_dpad2 ? {~ana_2[7],  ana_2[6:0]}  : JOY2_LEFT  ? 8'd4 : JOY2_RIGHT ? 8'd252 : 8'd128;
+			JOY2_Y <= ~use_dpad2 ? {~ana_2[15], ana_2[14:8]} : JOY2_UP    ? 8'd4 : JOY2_DOWN  ? 8'd252 : 8'd128;
 
 			CLK_DIV <= 1;
 		end
 
-		if (CLK_DIV==265) begin
+		if (CLK_DIV==100) begin
 			CLK_DIV <= 0;
-			if (JOY1_X>0) JOY1_X <= JOY1_X - 1'b1;
-			if (JOY1_Y>0) JOY1_Y <= JOY1_Y - 1'b1;
-			if (JOY2_X>0) JOY2_X <= JOY2_X - 1'b1;
-			if (JOY2_Y>0) JOY2_Y <= JOY2_Y - 1'b1;
+			if (JOY1_X) JOY1_X <= JOY1_X - 1'b1;
+			if (JOY1_Y) JOY1_Y <= JOY1_Y - 1'b1;
+			if (JOY2_X) JOY2_X <= JOY2_X - 1'b1;
+			if (JOY2_Y) JOY2_Y <= JOY2_Y - 1'b1;
 		end
 	end
 end
