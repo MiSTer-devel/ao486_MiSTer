b3f0b23a0a5787a8f566c8352f5f5cb9f07c412c
Pasi Lähteenmäki <decod81@gmail.com>
2022-02-27 17:38:45 +0200

    VGA scroll bug fix, now including fix for previously introduced color issue. (#104)

--- rtl/soc/vga.v
+++ rtl/soc/vga.v
@@ -1369,11 +1369,11 @@ assign dot_memory_load =
 	    (~(seq_8dot_char) && ~(seq_dotclock_divided) && dot_cnt_enable    && dot_cnt == 4'd4) ||
 	    (~(seq_8dot_char) && seq_dotclock_divided    && ~(dot_cnt_enable) && dot_cnt == 4'd7)
 	) &&
-	(   (vert_cnt == crtc_vertical_total + 1'd1 && horiz_cnt >= crtc_horizontal_total + 8'd3) ||
-	    (vert_cnt < crtc_vertical_display_size && (horiz_cnt <= crtc_horizontal_display_size - 8'd2 || horiz_cnt >= crtc_horizontal_total + 8'd3)) ||
-	    (vert_cnt == crtc_vertical_display_size && horiz_cnt <= crtc_horizontal_display_size - 8'd2)
+	( (vert_cnt == crtc_vertical_total + 1'd1) ||
+	  (vert_cnt < crtc_vertical_display_size) ||
+	  (vert_cnt == crtc_vertical_display_size)
 	);
-    
+
 assign dot_memory_load_first_in_frame = dot_memory_load && vert_cnt == crtc_vertical_total + 1'd1 && horiz_cnt == crtc_horizontal_total + 8'd3;
 assign dot_memory_load_first_in_line  = dot_memory_load && horiz_cnt == crtc_horizontal_total + 8'd3;
 assign dot_memory_load_first_in_line_matched =
